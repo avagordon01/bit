785794c39d2a41cc50c4a259ac7dbd5321bf660c
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-09-29 22:25:24 +0100

    windows ci

--- test.sh
+++ test.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
+if ! meson --version; then
+    pip3 install meson
+    #pip3 install --user meson
+    #export PATH=$PATH:~/.local/bin
+fi
 if [ ! -d subprojects ]; then
     mkdir -p subprojects
     meson wrap install gtest
 fi
 
 rm -rf out
-mkdir -p out
-compilers="clang++ g++"
-for compiler in $compilers; do
-    outdir="out/$compiler"
-    if [ ! -d "$outdir" ]; then
-        CXX=$compiler \
-        meson $outdir
-    fi
-    meson test -C $outdir --print-errorlogs
-done
+meson out
+meson test -C out --print-errorlogs
